12e91d225e2da94987c93d2ebf7ada4bd34662db
Cole Pratte <127376939+cpxx9@users.noreply.github.com>
2024-01-31 10:23:19 -0500

    added image url for town object

--- JS/scripts.js
+++ JS/scripts.js
@@ -46,7 +46,8 @@ const locations = [
     name: "town square",
     "button text": ["Go to store", "Go to cave", "Fight dragon"],
     "button functions": [goStore, goCave, fightDragon],
-    text: "You are in the town square. You see a sign that says \"Store\"."
+    text: "You are in the town square. You see a sign that says \"Store\".",
+    backgroundImage: "url('./img/town.jpg')"
   },
   {
     name: "store",
