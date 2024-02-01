cb8f0ee6e91853e1f895d26d3f43f965d9de40d6
Cole Pratte <127376939+cpxx9@users.noreply.github.com>
2024-02-01 12:14:43 -0500

    fixed easter egg bug

--- JS/scripts.js
+++ JS/scripts.js
@@ -95,6 +95,12 @@ const locations = [
     "button text": ["2", "8", "Go to town square?"],
     "button functions": [pickTwo, pickEight, goTown],
     text: "You find a secret game. Pick a number above. Ten numbers will be randomly chosen between 0 and 10. If the number you choose matches one of the random numbers, you win!"
+  },
+  {
+    name: "ee finish",
+    "button text": ["Go to town square", "Go to town square", "Go to town square"],
+    "button functions": [goTown, goTown, goTown],
+    text: 'Time to get back to it.'
   }
 ];
 
@@ -282,18 +288,21 @@ function pick(guess) {
   }
   text.innerText = "You picked " + guess + ". Here are the random numbers:\n";
   for (let i = 0; i < 10; i++) {
-    text.innerText += numbers[i] + "\n";
+    text.innerText += numbers[i] + ", ";
   }
   if (numbers.includes(guess)) {
-    text.innerText += "Right! You win 20 gold!";
     gold += 20;
     goldText.innerText = gold;
+    update(locations[8]);
+    text.innerText = "Right! You win 20 gold!";
   } else {
-    text.innerText += "Wrong! You lose 10 health!";
     health -= 10;
     healthText.innerText = health;
     if (health <= 0) {
       lose();
+    } else {
+      update(locations[8]);
+      text.innerText = "Wrong! You lose 10 health!";
     }
   }
 }
